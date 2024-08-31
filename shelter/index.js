5afc40da7f348f029ab765626830673f1150ae05
jsemenborodas <jsemenborodas@gmail.com>
2024-08-31 15:49:10 +0300

    feat: add burger-menu

--- shelter/index.js
+++ shelter/index.js
@@ -1,1 +1,38 @@
-console.log('Main:\n1.Проверка вёрстки:\n1)Верстка страницы валидная: +4\n2)Логотип в хедере состоит из текстовых элементов: +1\n3)Страница содержит ровно один элемент h1: +1\n4)добавлена favicon: +1\nИтог: 7\nВерстка соответствует макету: +35\ncss-требования: +6\nинтерактивность элементов:');
+let link = document.querySelector('#burger__link');
+let list = document.querySelector('#burger__list');
+let body = document.querySelector('.page');
+let burgerlinks = document.querySelectorAll('.burger__list__link');
+
+link.addEventListener('click', () => {
+    if(link.classList.contains('burger__link__active')){
+        link.classList.remove('burger__link__active');
+        list.classList.remove('burger__list__active');
+        body.classList.remove('burger__page__active');
+    }else{
+        link.classList.add('burger__link__active');
+        list.classList.add('burger__list__active');
+        body.classList.add('burger__page__active');
+    }
+})
+
+burgerlinks.forEach((burgerlink) => {
+    burgerlink.addEventListener('click', () => {
+        if(link.classList.contains('burger__link__active')){
+            link.classList.remove('burger__link__active');
+            list.classList.remove('burger__list__active');
+            body.classList.remove('burger__page__active');
+        }else{
+            link.classList.add('burger__link__active');
+            list.classList.add('burger__list__active');
+            body.classList.add('burger__page__active');
+        }
+    })
+})
+
+body.addEventListener('click', () => {
+    if(link.classList.contains('burger__link__active')){
+        link.classList.remove('burger__link__active');
+        list.classList.remove('burger__list__active');
+        body.classList.remove('burger__page__active');
+    }
+})
